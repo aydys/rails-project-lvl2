a754bb78565d922a407c5451f4e1e5aaa600579e
aidys23 <aidongak23@gmail.com>
2022-01-06 14:48:00 +0700

    add seeds for PostCategory

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+['ruby', 'rails', 'postgres'].each do | item |
+  PostCategory.create(name: item)
+end
