734590e74cc2bb2294d0cbb2569e031c9797d686
aidys23 <aidongak23@gmail.com>
2022-01-17 22:36:54 +0700

    i18n active_record

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,6 @@ class Post < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
+
+  validates :title, presence: true
 end
