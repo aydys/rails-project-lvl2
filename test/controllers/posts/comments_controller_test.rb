b0881f02e51075f0143c321bf0e492270ca0cd11
aidys23 <aidongak23@gmail.com>
2022-01-17 10:09:10 +0700

    add test for comments_controller

--- test/controllers/posts/comments_controller_test.rb
+++ test/controllers/posts/comments_controller_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class Posts::CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:one)
+  end
+
+  test '#create' do
+    sign_in users(:one)
+    post post_comments_url @post, params: { post_comment: { content: 'Test' } }
+
+    assert_redirected_to post_url @post
+  end
 end
