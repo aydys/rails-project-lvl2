a621725ad430a283dd0b6e949e102a4a857bd8ec
aidys23 <aidongak23@gmail.com>
2022-01-18 09:33:16 +0700

    add check user sign in to like

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -2,8 +2,12 @@
 
 class Posts::LikesController < ApplicationController
   def create
-    PostLike.create(post_id: params[:post_id], user_id: current_user.id)
-    redirect_to post_path(params[:post_id])
+    if user_signed_in?
+      PostLike.create(post_id: params[:post_id], user_id: current_user.id)
+      redirect_to post_path(params[:post_id])
+    else
+      redirect_to new_user_session_path, notice: t('devise.failure.unauthenticated')
+    end
   end
 
   def destroy
