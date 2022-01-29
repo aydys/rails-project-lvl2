64cf352ecb7c0d5aa79de57c57d7a86eb8137aca
aidys23 <aidongak23@gmail.com>
2022-01-29 18:46:58 +0700

    i18n alert & notice

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -4,17 +4,22 @@ class Posts::CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @comment = PostComment.new(post_id: params[:post_id], user_id: current_user.id, **comment_params)
+    post_id = params[:post_id]
+    @comment = PostComment.new(post_id: post_id, user_id: current_user.id, **comment_params)
 
     if @comment.save
-      redirect_to post_path(params[:post_id]), notice: 'Comment created successfully'
+      redirect_to post_path(post_id), notice: t('.comment_created')
     else
-      @post = Post.find(params[:post_id])
-      render 'posts/show', alert: 'Comment not created', status: :unprocessable_entity
+      render_post_with_alert
     end
   end
 
   private
+
+  def render_post_with_alert
+    @post = Post.find(params[:post_id])
+    render 'posts/show', alert: t('.comment_failed'), status: :unprocessable_entity
+  end
 
   def comment_params
     params.require(:post_comment).permit(:content, :parent_id)
